7743eee5672a13fe3689ee8506838ee963f72ae5
Aleksandar Todorov <support@alexandertodorov.com>
2022-08-21 13:12:34 +0300

    update unit test and add codecov config

--- tests/NativeSessionHandlerTest.php
+++ tests/NativeSessionHandlerTest.php
@@ -3,10 +3,18 @@
 namespace SSSession\UnitTest;
 
 use PHPUnit\Framework\TestCase;
+use RuntimeException;
 use SSSession\NativeSessionHandler;
 
 class NativeSessionHandlerTest extends TestCase
 {
+    protected function tearDown(): void
+    {
+        if (\is_dir(__DIR__ . '/tmpnoaccess')) {
+            \rmdir(__DIR__ . '/tmpnoaccess');
+        }    
+    }
+
     public function testCreationOfSessionDir(): void
     {
         $path = __DIR__ . 'session';
@@ -25,5 +33,15 @@ class NativeSessionHandlerTest extends TestCase
         $this->assertDirectoryExists(__DIR__ . 'tmp');
 
         \rmdir(__DIR__ . 'tmp');
+    }
+
+    public function testDirWithNoAccess(): void
+    {
+        $dir = __DIR__ . '/tmpnoaccess';
+        \mkdir($dir, 0100);
+
+        $this->expectException(RuntimeException::class);
+
+        new NativeSessionHandler($dir . '/session');
     }
 }
